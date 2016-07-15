eb0ef403f445e496a5cfa68ac212ba5896b8c4cc
Undergrid <nick-github@undergrid.org.uk>
2016-07-15 16:19:02 +0100

    Initial Commit

---
+++ firmware/examples/ExampleListener.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "JsonListener.h"
+
+class ExampleListener: public JsonListener {
+
+  public:
+    void whitespace(char c);
+
+    void value(String key, String value);
+
+    void startDocument();
+    void endDocument();
+
+    void startArray(String key);
+    void endArray();
+    
+    void startObject(String key);
+    void endObject();
+}
